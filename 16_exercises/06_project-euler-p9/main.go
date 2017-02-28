05b27ab5c4b0e0e8b6a6187c37fe1c93cbcfb92f
Serhii <s.lehkyi@cronostelecom.com>
2017-02-28 17:17:17 +0100

    updates to project euler

--- 16_exercises/06_project-euler-p9/main.go
+++ 16_exercises/06_project-euler-p9/main.go
@@ -14,7 +14,20 @@ func main() {
 			c = math.Sqrt(a*a+b*b)
 			if a < b && b < c && a + b + c == 1000 {
 				fmt.Println(a, b, c)
+				fmt.Println(a*b*c)
 			}
 		}
 	}
 }
+
+/*
+Special Pythagorean triplet
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+ */
